b1cda74c290e76466a216d122fe00f81a8162608
p.semin <p.semin@city-mobil.ru>
2020-12-29 21:35:01 +0300

    create SightCard

---
+++ lib/ui/screen/sight_card.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+import 'package:places/res/colors.dart';
+import 'package:places/res/dimens.dart';
+
+class SightCard extends StatelessWidget {
+  final Sight _sight;
+
+  SightCard(this._sight);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: Dimens.margin16),
+      child: Column(
+        children: [
+          Container(
+            height: Dimens.sightCardImageHeight,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.indigo,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimens.sightCardCornerRadius),
+                topRight: Radius.circular(Dimens.sightCardCornerRadius),
+              ),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(Dimens.padding16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _sight.type.toLowerCase(),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: Dimens.textMaxLines1,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: Dimens.fontSize14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      width: Dimens.sightCardIconSize,
+                      height: Dimens.sightCardIconSize,
+                      color: Colors.greenAccent,
+                    )
+                  ],
+                )),
+          ),
+          Container(
+            height: Dimens.sightCardInfoSectionHeight,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(AppColors.wildSand),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(Dimens.sightCardCornerRadius),
+                  bottomRight: Radius.circular(Dimens.sightCardCornerRadius)),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(Dimens.padding16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: Dimens.margin2),
+                    child: Text(
+                      _sight.name,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: Dimens.textMaxLines2,
+                      style: TextStyle(
+                          color: Color(AppColors.oxfordBlue),
+                          fontSize: Dimens.fontSize16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Text(
+                    _sight.details,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: Dimens.textMaxLines1,
+                    style: TextStyle(
+                        color: Color(AppColors.waterloo),
+                        fontSize: Dimens.fontSize14,
+                        fontWeight: FontWeight.normal),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:places/res/colors.dart';
 import 'package:places/res/dimens.dart';
 import 'package:places/res/strings.dart';
+import 'package:places/ui/screen/sight_card.dart';
+
+import '../../mocks.dart';
 
 /// Main screen of the application
 class SightListScreen extends StatefulWidget {
@@ -15,26 +18,36 @@ class _SightListScreenState extends State<SightListScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        toolbarHeight: sightListScreenToolbarHeight,
-        elevation: elevation0,
+        toolbarHeight: Dimens.sightListScreenToolbarHeight,
+        elevation: Dimens.elevation0,
         backgroundColor: Colors.transparent,
         brightness: Brightness.light,
         title: Container(
-          height: sightListScreenToolbarHeight,
+          height: Dimens.sightListScreenToolbarHeight,
           alignment: Alignment.bottomLeft,
           child: Text(
-            sightListScreenTitle,
+            Strings.sightListScreenTitle,
             overflow: TextOverflow.ellipsis,
-            maxLines: textMaxLines2,
+            maxLines: Dimens.textMaxLines2,
             textAlign: TextAlign.start,
             style: TextStyle(
-              color: Color(oxfordBlue),
-              fontSize: fontSize32,
+              color: Color(AppColors.oxfordBlue),
+              fontSize: Dimens.fontSize32,
               fontWeight: FontWeight.bold,
             ),
           ),
         ),
       ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(Dimens.padding16),
+        child: Column(
+          children: getSightCards(),
+        ),
+      ),
     );
   }
+
+  List<Widget> getSightCards() =>
+      mocks.map((sight) => SightCard(sight)).toList();
 }
